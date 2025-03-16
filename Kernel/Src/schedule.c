9713dcc2af0c59259ed991664cd8c984562e1623
hanhanhan66615 <2016530262@qq.com>
2025-03-16 16:57:08 +0800

    second commit

--- Kernel/Src/schedule.c
+++ Kernel/Src/schedule.c
@@ -35,7 +35,7 @@ void rt_system_scheduler_init(void)
 #else
     register rt_base_t offset;
 
-    for(offset = 0; offset < RT_THREAD_PRIORITY_MAX; offset++)
+    for (offset = 0; offset < RT_THREAD_PRIORITY_MAX; offset++)
     {
         rt_list_init(&rt_thread_priority_table[offset]);
     }
@@ -75,9 +75,9 @@ void rt_system_scheduler_start(void)
     highest_ready_priority = __rt_ffs(rt_thread_ready_priority_group) - 1;
 
     to_thread = rt_list_entry(rt_thread_priority_table[highest_ready_priority].next,
-                             struct rt_thread,
-                             tlist);
-    rt_current_thread = to_thread;    
+                              struct rt_thread,
+                              tlist);
+    rt_current_thread = to_thread;
 
     /* 切换到新线程 */
     rt_hw_context_switch_to((rt_uint32_t)&to_thread->sp);
@@ -87,7 +87,7 @@ void rt_system_scheduler_start(void)
 void rt_schedule_insert_thread(struct rt_thread *thread)
 {
     /* register修饰的变量，如果可能得话应尽量保存在cpu的寄存器中,以加快存储速度 */
-    register rt_base_t temp; 
+    register rt_base_t temp;
 
     /* 关中断 */
     temp = rt_hw_interrupt_disable();
@@ -97,7 +97,7 @@ void rt_schedule_insert_thread(struct rt_thread *thread)
 
     /* 将线程插入就绪列表 */
     rt_list_insert_before(&(rt_thread_priority_table[thread->current_priority]),
-                        &(thread->tlist));
+                          &(thread->tlist));
 
     /* 设置线程就绪优先级组中对应的位 */
     rt_thread_ready_priority_group |= thread->number_mask;
@@ -110,7 +110,7 @@ void rt_schedule_insert_thread(struct rt_thread *thread)
 void rt_thread_remove_thread(struct rt_thread *thread)
 {
     /* register修饰的变量，如果可能得话应尽量保存在cpu的寄存器中,以加快存储速度 */
-    register rt_base_t temp; 
+    register rt_base_t temp;
 
     /* 关中断 */
     temp = rt_hw_interrupt_disable();
@@ -118,7 +118,7 @@ void rt_thread_remove_thread(struct rt_thread *thread)
     /* 将线程从就绪队列中删除 */
     rt_list_remove(&(thread->tlist));
 
-    if(rt_list_isempty(&(rt_thread_priority_table[thread->current_priority])))
+    if (rt_list_isempty(&(rt_thread_priority_table[thread->current_priority])))
         rt_thread_ready_priority_group &= ~thread->number_mask;
 
     /* 开中断 */
@@ -211,7 +211,7 @@ void rt_schedule(void)
             }
         }
     }
-    #else
+#else
     rt_base_t level;
     register rt_ubase_t highest_ready_priority;
     struct rt_thread *to_thread;
@@ -220,33 +220,24 @@ void rt_schedule(void)
     /* 关中断 */
     level = rt_hw_interrupt_disable();
 
-    /* 获取就绪的最高优先级 */                         
+    /* 获取最高优先级就绪线程 */
     highest_ready_priority = __rt_ffs(rt_thread_ready_priority_group) - 1;
-    /* 获取就绪的最高优先级对应的线程控制块 */             
     to_thread = rt_list_entry(rt_thread_priority_table[highest_ready_priority].next,
-                                struct rt_thread,
-                                tlist);
+                              struct rt_thread, tlist);
 
-    /* 如果目标线程不是当前线程，则要进行线程切换 */
-    if (to_thread != rt_current_thread)              
+    /* 仅在不同线程时切换 */
+    if (to_thread != rt_current_thread)
     {
         rt_current_priority = (rt_uint8_t)highest_ready_priority;
-        from_thread         = rt_current_thread;
-        rt_current_thread   = to_thread;
+        from_thread = rt_current_thread;
+        rt_current_thread = to_thread;
 
-        rt_hw_context_switch((rt_uint32_t)&from_thread->sp,
-                            (rt_uint32_t)&to_thread->sp);
-
-        /* 开中断 */
-        rt_hw_interrupt_enable(level);
-
+        /* 触发上下文切换（保持中断关闭） */
+        rt_hw_context_switch((rt_uint32_t)&from_thread->sp, (rt_uint32_t)&to_thread->sp);
     }
-    else
-    {
-        /* 开中断 */
-        rt_hw_interrupt_enable(level);
-    }
+    /* 统一恢复中断 */
+    rt_hw_interrupt_enable(level);
 #endif
-    /* 产生上下文切换 */
-    rt_hw_context_switch((rt_uint32_t)&from_thread->sp, (rt_uint32_t)&to_thread->sp);
+    /* 触发上下文切换（保持中断关闭） */
+    //rt_hw_context_switch((rt_uint32_t)&from_thread->sp, (rt_uint32_t)&to_thread->sp);
 }
